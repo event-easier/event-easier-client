228fa33de036c09e0a4279f8e57c86ff002dde32
baokhanh1701 <khanhnkb@gmail.com>
2023-05-12 22:36:02 +0700

    backup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from './pages/LandingPage'
@@ -23,43 +19,49 @@ import CalendarSettings from './pages/CalendarSettings'
 import EventSettings from './pages/EventSettings'
 import ErrorPage from './pages/ErrorPage'
 
+const routes = [
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+
+]
+
 function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Main Routes */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/event/:eid" element={<EventPage />} />
-          {/*Authentication*/}
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          
-          {/* Routes for Home */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/home/calendars" element={<Calendars />} />
-          <Route path="/personal" element={<Personal />} />
-          <Route path="/discover" element={<DiscoverEvents />} />
-          
-          {/* Routes for User */}
-          <Route path="/user/:uid/profile" element={<UserProfile />} />
-          <Route path="/user/:uid/settings" element={<Settings />} />
-          
-          {/* Routes for Event Manager */}
-          <Route path="/events-manager" element={<EventsManager />} />
-          <Route path="/create-event" element={<CreateEvent />} />
-          <Route path="/event-settings" element={<EventSettings />} />
-          
-          {/* Routes for Calendar Manager */}
-          <Route path="/calendars-manager" element={<CalendarsManager />} />
-          <Route path="/create-calendars" element={<CreateCalendar />} />
-          <Route path="/calendar-settings" element={<CalendarSettings />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Main Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/event/:eid" element={<EventPage />} />
+        {/*Authentication*/}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
 
-          {/* 404 */}
-          <Route path="/*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+        {/* Routes for Home */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/calendars" element={<Calendars />} />
+        <Route path="/personal" element={<Personal />} />
+        <Route path="/discover" element={<DiscoverEvents />} />
+
+        {/* Routes for User */}
+        <Route path="/user/:uid/profile" element={<UserProfile />} />
+        <Route path="/user/:uid/settings" element={<Settings />} />
+
+        {/* Routes for Event Manager */}
+        <Route path="/events-manager" element={<EventsManager />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/event-settings" element={<EventSettings />} />
+
+        {/* Routes for Calendar Manager */}
+        <Route path="/calendars-manager" element={<CalendarsManager />} />
+        <Route path="/create-calendars" element={<CreateCalendar />} />
+        <Route path="/calendar-settings" element={<CalendarSettings />} />
+
+        {/* 404 */}
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
