fcc2c019581109fff8cab9a1a0fbb0fe7152b8e1
baokhanh1701 <khanhnkb@gmail.com>
2023-06-13 20:09:32 +0700

    Minor fix

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -35,60 +35,51 @@ export function useWindowDimensions() {
 export const AppContext = React.createContext("");
 
 export default function AppProvider({ children }) {
-  const navigate = useNavigate();
-  const { width, height } = useWindowDimensions();
-  const commonBreakPoint = [320, 480, 768, 1024, 1025, 1200];
-  // const profile_data = JSON.parse(localStorage.getItem("profile-data")).data.data;
+    const navigate = useNavigate();
+    const { width, height } = useWindowDimensions();
+    const commonBreakPoint = [320, 480, 768, 1024, 1025, 1200];
+    const [events, setEvents] = useState([])
+    const [initRenderFlag, setInitRenderFlag] = useState(true)
 
-  const [events, setEvents] = useState([]);
-  const [upcomingEvents, setUpComingEvents] = useState([]);
-  const [pastEvents, setPastEvents] = useState([]);
-  const [initRenderFlag, setInitRenderFlag] = useState(true);
+    const upcomingEvents = events.filter((event) => {
+        const end_time = event.end_time;
+        return new Date().valueOf() < new Date(end_time).valueOf()
+    })
 
-  const checkEventTime = (event) => {
-    const current = new Date().getTime();
-    const event_start = new Date(event.start_time);
-    if (event_start >= current && initRenderFlag) {
-      setUpComingEvents((current) => [...current, event]);
-    } else if (event_start < current && initRenderFlag) {
-      setPastEvents((current) => [...current, event]);
+    const pastEvents = events.filter((event) => {
+        const end_time = event.end_time;
+        return new Date().valueOf() > new Date(end_time).valueOf()
+    })
+
+    const fetchEventsData = async () => {
+        const id = JSON.parse(localStorage.getItem('profile-data'))?.data?.data._id
+        const events_update = await getAllEvents(
+            {
+                user_id: id
+            }
+        )
+        setEvents(events_update.data)
     }
-  };
 
-  const fetchEventsData = async () => {
-    const id = JSON.parse(localStorage.getItem("profile-data"))._id;
-    const events_update = await getAllEvents({
-      user_id: id,
-    });
-    setEvents(events_update.data);
-    events_update.data.map((event) => {
-      checkEventTime(event);
-    }); //This is a bad fix of getting upcoming_events state and past_events state, very bad code, but it works! :)
-  };
-
-  useEffect(() => {
-    setInitRenderFlag(true);
-    fetchEventsData();
-    setInitRenderFlag(false);
-  }, []); //Get events state
-  return (
-    <AppContext.Provider
-      value={{
-        width,
-        height,
-        commonBreakPoint,
-        navigate,
-        events,
-        setEvents,
-        upcomingEvents,
-        setUpComingEvents,
-        pastEvents,
-        setPastEvents,
-        initRenderFlag,
-        setInitRenderFlag,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+    useEffect(() => {
+        setInitRenderFlag(true);
+        fetchEventsData();
+        setInitRenderFlag(false);
+    }, [])  //Get events state
+    return (
+        <AppContext.Provider
+            value={{
+                width,
+                height,
+                commonBreakPoint,
+                navigate,
+                events, setEvents,
+                upcomingEvents,
+                pastEvents,
+                initRenderFlag, setInitRenderFlag
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
 }
